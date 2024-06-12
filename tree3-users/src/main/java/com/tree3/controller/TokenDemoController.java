4be6b31c16427e03493c8df53321d44ad241ba48
Tree3life <749328583@qq.com>
2024-06-12 20:10:34 +0800

    docs(TokenDemoController): 注释

--- tree3-users/src/main/java/com/tree3/controller/TokenDemoController.java
+++ tree3-users/src/main/java/com/tree3/controller/TokenDemoController.java
@@ -24,7 +24,7 @@ public class TokenDemoController {
   @GetMapping("/token")
   public String getToken() {
       // 获取用户信息（这里使用模拟数据）
-      // 注：这里存储该内容只是举例，其作用为辅助验证，使其验证逻辑更安全，如这里存储用户信息，其目的为:
+      // 注：这里存储该内容只是举例，其作用为辅助验证，使其验证逻辑更安全，比如在这里存储用户信息，的 目的是:
       // - 1)、使用"token"验证 Redis 中是否存在对应的 Key
       // - 2)、使用"用户信息"验证 Redis 的 Value 是否匹配。
       String userInfo = "mydlq";
